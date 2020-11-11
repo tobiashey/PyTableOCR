167fd032cefa231e055dee487c0c9f8332d4064e
Tobias <45271630+tobiashey@users.noreply.github.com>
2020-11-11 21:49:56 +0100

    added Pandas Output

--- tesseractTest.py
+++ tesseractTest.py
@@ -3,13 +3,17 @@ try:
 except ImportError:
     import Image
 import pytesseract
+from pytesseract import Output
+
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
-print(pytesseract.image_to_data(Image.open('images/Excel/ExcelTabelle.png')))
+
+pytesRes = pytesseract.image_to_data(Image.open('images/Excel/ExcelTabelle.png'), output_type=Output.DATAFRAME)
+print(type(pytesRes))
 
 # image_to_string => String of text
 # image_to_boxes => position of chars
-# image_to_data => level, page_num, block_num, par_num, line_num, word_num	left, top, width, height, conf, text
+# image_to_data => level, page_num, block_num, par_num, line_num, word_num, left, top, width, height, conf, text
 # image_to_osd => Some Data about the Image
 
 # ExcelTabelle.png
